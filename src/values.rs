95530dca9d6e722865303bbab27622c574fa2127
tbrunain <dev@bouh.io>
2021-09-21 20:37:45 +0200

    fix: Issue withg dynamic array

--- src/values.rs
+++ src/values.rs
@@ -326,7 +326,7 @@ impl Value {
                     .ok_or_else(|| anyhow!("reached end of input while decoding array length"))?;
                 let array_len = U256::from_big_endian(slice).as_usize();
 
-                let (arr, _) = Self::decode(bs, &Type::FixedArray(ty.clone(), array_len), at, 32)?;
+                let (arr, _) = Self::decode(bs, &Type::FixedArray(ty.clone(), array_len), 32, 0)?;
 
                 let values = if let Value::FixedArray(values, _) = arr {
                     values
